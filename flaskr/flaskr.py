7d23f791bc813d89db93f449d890254f6448d97d
Wei Xu <ginues109@gmail.com>
2018-04-09 23:14:16 -0700

    wip

---
+++ flaskr/flaskr.py
@@ -0,0 +1,23 @@
+import os
+import sqlite3
+from flask import Flask
+
+app = Flask(__name__)
+# TODO print out the name of this app ?
+app.config.from_object(__name__)
+
+app.config.update(dict(
+    DATABASE=os.path.join(app.root_path, 'flaskr.db'),
+    SECRET_KEY='development key',
+    USERNAME='admin',
+    PASSWORD='default'
+))
+
+app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+
+
+def connect_db():
+    """Connects to the specific database."""
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
