c9adb0cc3783d68539360ffacdf8f2b1f0cd3278
Wei Xu <ginues109@gmail.com>
2018-05-05 18:46:49 -0700

    update to use executescript

--- flaskr/db.py
+++ flaskr/db.py
@@ -25,7 +25,7 @@ def init_db():
     db = get_db_connection()
 
     with current_app.open_resource('schema.sql') as f:
-        db.execute(f.read().decode('utf8'))
+        db.executescript(f.read().decode('utf8'))
 
 
 @click.command('init-db')
